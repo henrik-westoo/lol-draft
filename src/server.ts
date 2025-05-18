953a2518c2619be9802e0fa1d996e60efb96a424
henrikwestoo <henrik.westoo@etimo.se>
2025-05-18 13:24:30 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import { DraftManager } from "./services/draft-manager.js";
-import { CHAT_INPUT_COMMANDS } from "./interactions/chat-input/index.js";
 import { readButtonId } from "./utils/button-id.js";
 import { DraftRepository } from "./repositories/draft-repository.js";
-import { BUTTON_COMMANDS } from "./interactions/button/index.js";
 import { registerCommands } from "./registerCommands.js";
 
 dotenv.config();
 // import redis after setting up dotenv
 import { redis } from "./redis.js";
+import { buildDraftEmbed, buildPlayerButtons } from "./ui/ui.js";
 
 redis.ping().then((res) => {
 	console.log("Redis ping:", res);
@@ -28,6 +27,8 @@ await registerCommands();
 client.once("ready", (readyClient) => {
 	console.log(`🤖 Logged in as ${readyClient.user.tag}`);
 });
+
+const activeDrafts = new Set<string>(); // channel IDs in progress
 
 client.on("interactionCreate", async (interaction) => {
 	if (!interaction.isChatInputCommand() && !interaction.isButton()) {
@@ -50,13 +51,61 @@ client.on("interactionCreate", async (interaction) => {
 
 	if (interaction.isChatInputCommand()) {
 		console.log(
-			`📜 Chat command: ${interaction.commandName}, issued by ${interaction.user.globalName}`,
+			`📜 Chat command: ${interaction.commandName}, issued by ${interaction.user.globalName} (${interaction.user.id})`,
 		);
 		switch (interaction.commandName) {
-			case "startdraft":
-				return CHAT_INPUT_COMMANDS.startDraft(interaction, draftManager);
-			case "canceldraft":
-				return CHAT_INPUT_COMMANDS.cancelDraft(interaction, draftManager);
+			case "startdraft": {
+				await interaction.reply("Starting draft...");
+
+				// bots dont have globalName
+				if (interaction.options.data.some((option) => !option.user?.globalName))
+					return interaction.editReply({
+						content:
+							"One user is missing globalName, cannot start draft. Maybe thet user was a bot?",
+					});
+
+				const initDraftResponse = await draftManager.initDraft({
+					players: interaction.options.data.map((option) => {
+						return {
+							id: option.user!.id,
+							name: option.user!.globalName!,
+						};
+					}),
+				});
+
+				if (typeof initDraftResponse === "string") {
+					let msg = "❌ ";
+
+					switch (initDraftResponse) {
+						case "draft-already-in-progress":
+							msg += "Draft already in progress.";
+							break;
+						case "not-enough-players":
+							msg += "Not enough players.";
+							break;
+						case "duplicate-players":
+							msg += "Duplicate players.";
+							break;
+					}
+					return interaction.editReply({
+						content: msg,
+					});
+				}
+
+				return interaction.editReply({
+					embeds: [buildDraftEmbed(initDraftResponse)],
+					content: `<@${initDraftResponse.turnOrder[initDraftResponse.currentTurnIndex]}>, your turn to pick!`,
+					components: buildPlayerButtons(initDraftResponse.availablePlayers),
+				});
+			}
+			case "canceldraft": {
+				await interaction.reply("Cancelling draft...");
+				const cancelDraftResponse = await draftManager.cancelDraft();
+				if (typeof cancelDraftResponse === "string") {
+					return interaction.editReply(cancelDraftResponse);
+				}
+				return interaction.editReply("Draft cancelled!");
+			}
 			default:
 				return interaction.reply({
 					content: `Unknown command: ${interaction.commandName}`,
@@ -71,8 +120,45 @@ client.on("interactionCreate", async (interaction) => {
 		);
 
 		switch (prefix) {
-			case "pick":
-				return BUTTON_COMMANDS.pickPlayer(interaction, draftManager, id);
+			case "pick": {
+				const pickPlayerResponse = await draftManager.pickPlayer({
+					captainId: interaction.user.id,
+					playerId: id,
+				});
+
+				if (typeof pickPlayerResponse === "string") {
+					switch (pickPlayerResponse) {
+						case "draft-not-found":
+							return interaction.reply({
+								content: "❌ Draft not found.",
+							});
+						case "draft-not-in-progress":
+							return interaction.reply({
+								content: "❌ Draft is not in progress.",
+							});
+						case "player-not-available":
+							return interaction.reply({
+								content: "❌ Player is not available.",
+							});
+						case "invalid-captain-turn":
+							return interaction.reply({
+								content:
+									"❌ Invalid action. Either you are not the captain or it is not your turn.",
+							});
+					}
+				}
+
+				const playerName =
+					pickPlayerResponse!.turnOrder[pickPlayerResponse!.currentTurnIndex];
+
+				return interaction.update({
+					content: playerName
+						? `<@${playerName}>, your turn!`
+						: "All done! Thank you for using donger-draft.",
+					embeds: [buildDraftEmbed(pickPlayerResponse!)],
+					components: buildPlayerButtons(pickPlayerResponse!.availablePlayers),
+				});
+			}
 			default:
 				return interaction.reply({
 					content: `Unknown button command: ${interaction.customId}`,
